eb9a22b9bd3f8b192a545612ea9cdae5e6e7ed59
Jagadeesh Varma <jagadeesh.ifs@gmail.com>
2018-09-02 10:40:45 +0530

    jagadeesh varma

--- assignment3.py
+++ assignment3.py
@@ -1,4 +1,5 @@
 #Reverse the number
+#This is my change - jagadeesh varma
 
 rNum=int(input('Enter a number to be reversed'))
 
@@ -6,4 +7,3 @@ while(rNum!=0):
     res=rNum%10
     rNum=rNum/10
     print(res)
-    
