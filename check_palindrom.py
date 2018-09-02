b78fbc3d5bc88b40d8b67c9e49c655a9da0341d2
Jagadeesh Varma <jagadeesh.ifs@gmail.com>
2018-09-02 12:04:06 +0530

    check if the given number is palindrom or not

---
+++ check_palindrom.py
@@ -0,0 +1,7 @@
+# Palindrom:
+# a word, phrase, or sequence that reads the same backwards as forwards,
+# e.g. madam or nurses run.
+#
+# In this excercise we only check numbers.
+# e.g. n = 1221 is palindrom
+#      n = 1234 is not palindrom
