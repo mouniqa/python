b799e4a65f0d66711c55ef6daca3e0b3c34cd1f4
Mounika Varma <mouny.jv@gmail.com>
2018-09-02 12:21:47 +0530

    Checking whether given number is Palindrom or not

--- check_palindrom.py
+++ check_palindrom.py
@@ -5,3 +5,18 @@
 # In this excercise we only check numbers.
 # e.g. n = 1221 is palindrom
 #      n = 1234 is not palindrom
+
+rNum=int(input('Enter a number to be reversed : '))
+result=0
+storerNum=rNum
+while(rNum!=0):
+    digit=rNum%10
+    result=result*10+digit
+    rNum=int(rNum/10)
+
+print("Number got reversed :",result)
+
+if(storerNum == result):
+    print("Gven number is  a  Palindrom")
+else:
+    print("Given is not a Palindrom")
