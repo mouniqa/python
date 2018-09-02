5043e9d008403f3120942d2dd76d98c428def83a
Mounika Varma <mouny.jv@gmail.com>
2018-09-02 12:11:16 +0530

    sum_of_digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -4,3 +4,13 @@
 #       result = 1+2+3+4 = 10
 #     2: n=23
 #       result = 5
+
+sOfDigits=int(input("Enter number to be summed up of : "))
+
+res=0
+
+while(sOfDigits != 0):
+    temp=sOfDigits%10
+    res=res+temp
+    sOfDigits=int(sOfDigits/10)
+print("Summation of numbers is : ",res)
